2d31eb117ab0279c09c4e2e08ed909e6dd4355a9
Asimo <rvolenc@gmail.com>
2025-10-14 12:19:41 +0400

    init: install vite-react-ts template

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <div className="logo">Company</div>
+        <h2>Sign in to your account to continue</h2>
+        <form>
+          <input></input>
+          <input></input>
+          <button type="submit">Log in</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
